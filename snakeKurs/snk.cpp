d7fdc00a8db451d72673273a90f4a1ca5d488d87
rjwaliczek <r.j.waliczek@gmail.com>
2024-10-28 20:58:25 +0100

    snake tab

--- snakeKurs/snk.cpp
+++ snakeKurs/snk.cpp
@@ -6,34 +6,35 @@
 using namespace std;
 
 char wybor;
-int menu;
-int szerokosc, wysokosc, szybkosc;
-int *stol;
 
-void menu1();
-void menu2();
+int szerokosc, wysokosc, szybkosc;
+
+
+int menu1(int m);
+int menu2(int m);
 void latwy();
 void sredni();
 void trudny();
+void zwolnijPamiec(int **&tab, int szer);
 
 void pole(int **&tab,int szer,int wys);
 
 
 int main()
 {
-    
+    int menu;
 
     while(true)
     {
-        menu=1;
+         menu=1;
 
-        menu1();
+       
 
-        switch(menu)
+        switch(menu1(menu))
             {
                 case 1:
                 {
-                    menu2();
+                    menu2(menu);
                     break;
                 }
                 case 2:
@@ -42,7 +43,7 @@ int main()
                 }
             } 
 
-        switch(menu)
+        switch(menu2(menu))
             {
                 case 1:
                 {
@@ -61,7 +62,7 @@ int main()
                 }
                 case 4:
                 {
-                    break;
+                   break;
                 }
             } 
        
@@ -74,60 +75,64 @@ int main()
     return 0;
 }
 
-void menu1()
+int menu1(int m)
 {
      do
         {
             system("cls");
            cout << "\t\tSNAKE\n\n";
-           cout << (menu == 1 ? "->" : "  ") << "Graj" << endl;
-           cout << (menu == 2 ? "->" : "  ") << "wyjdz" << endl;
+           cout << (m == 1 ? "->" : "  ") << "Graj" << endl;
+           cout << (m == 2 ? "->" : "  ") << "wyjdz" << endl;
             wybor=getch();
-            if(wybor==72 && menu>1)
+            if(wybor==72 && m>1)
             {
-                menu--;
+                m--;
             }   
-            if(wybor==80 && menu<2)
+            if(wybor==80 && m<2)
             {
-                menu++;
+                m++;
             }     
         
         } while (wybor!=13);
+        return m;
 }
 
-void menu2()
+int menu2(int m)
 {
     do
     {
      system("cls");
            cout << "\t\tSNAKE\n\n";
-           cout << (menu == 1 ? "->" : "  ") << "Latwy" << endl;
-           cout << (menu == 2 ? "->" : "  ") << "Sredni" << endl;
-           cout << (menu == 3 ? "->" : "  ") << "Trudny" << endl;
-           cout << (menu == 4 ? "->" : "  ") << "Powrot" << endl;
+           cout << (m == 1 ? "->" : "  ") << "Latwy" << endl;
+           cout << (m == 2 ? "->" : "  ") << "Sredni" << endl;
+           cout << (m == 3 ? "->" : "  ") << "Trudny" << endl;
+           cout << (m == 4 ? "->" : "  ") << "Powrot" << endl;
             wybor=getch();
-            if(wybor==72 && menu>1)
+            if(wybor==72 && m>1)
             {
-                menu--;
+                m--;
             }   
-            if(wybor==80 && menu<4)
+            if(wybor==80 && m<4)
             {
-                menu++;
+                m++;
             }     
     } while (wybor!=13);
+    return m;
 }
 
 void latwy()
 {
+    int **stol = nullptr;
     szerokosc = 15;
     wysokosc = 15;
     szybkosc= 10;
     
-    while(true)
+    do
     {
         pole(stol,szerokosc,wysokosc);
 
-    }
+
+    }while(wybor=='q');
     
 }
 void sredni()
@@ -141,11 +146,23 @@ void trudny()
     system("pause");
 }
 
-void pole(int **tab,int szer,int wys)
+void pole(int **&tab,int szer,int wys)
 {
-    tab=new *int[szer];
+    tab=new int*[szer];
     for (int i=0; i<szer; i++)
     {
-        tab[szer]=new int[wys];
+        tab[i]=new int[wys];
     }
+
+
+    zwolnijPamiec(tab,szer);
+}
+
+void zwolnijPamiec(int **&tab, int szer)
+{
+    for(int i=0; i<szer; i++)
+    {
+        delete [] tab[i];
+    }
+    delete [] tab;
 }
